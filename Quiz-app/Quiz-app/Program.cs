872ee9194fb45964d68b0452959be3d13c0ae973
Matthijs <121012177+1Matthijs@users.noreply.github.com>
2024-09-10 10:13:44 +0200

    Login And Register

--- Quiz-app/Quiz-app/Program.cs
+++ Quiz-app/Quiz-app/Program.cs
@@ -1,2 +1,123 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Quiz_app.Data;
+using Quiz_app.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz_app
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            bool program = true;
+            using (var context = new QuizContext())
+            {
+               
+
+                while (program)
+                {
+                    Console.WriteLine("Welkom bij de StudentQuizApp!");
+                    Console.WriteLine("1. Inloggen");
+                    Console.WriteLine("2. Registreren");
+                    string choice = Console.ReadLine();
+
+                    if (choice == "1")
+                    {
+                        var user = Login(context);
+                        if (user != null)
+                        {
+                            if (user.IsTeacher)
+                            {
+                                Console.WriteLine("login as teacher succes");
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("login as Student succes");
+
+                            }
+                        }
+                    }
+                    else if (choice == "2")
+                    {
+                        Register(context);
+                    }
+                    else
+                    {
+                        program = false; 
+                    }
+                }
+            }
+        }
+
+        static User Login(QuizContext context)
+        {
+            bool login = true;
+            while (login)
+            {
+                Console.Write("Gebruikersnaam: ");
+                string username = Console.ReadLine();
+                Console.Write("Wachtwoord: ");
+                string password = Console.ReadLine();
+
+                var user = context.Users.SingleOrDefault(u => u.Username == username && u.Password == password);
+                if (user != null)
+                {
+                    Console.WriteLine("Inloggen succesvol!");
+                    login = false;
+                    return user;
+                }
+
+                Console.WriteLine("Ongeldige inloggegevens.");
+                
+            }
+            login = false;
+            return null;
+        }
+
+        static void Register(QuizContext context)
+        {
+            bool register = true;
+            string username = "";
+            string password = "";
+
+            while (register)
+            {
+                Console.Write("Gebruikersnaam: ");
+                username = Console.ReadLine();
+                Console.Write("Wachtwoord: ");
+                password = Console.ReadLine();
+
+                if(password == "" || username == "") {
+                    Console.WriteLine("geen geldige input");
+                }
+                else
+                {
+                    register = false;
+                    Console.WriteLine(password, username);
+                   
+                }                                   
+            }
+
+
+            var user = new User
+            {
+                Username = username,
+                Password = password,
+                IsTeacher = false
+            };
+
+            context.Users.Add(user);
+            context.SaveChanges();
+            Console.WriteLine("Registratie succesvol! Je kunt nu inloggen.");
+
+
+        }
+    }
+}
+
